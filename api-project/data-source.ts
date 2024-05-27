7fd366ff54bd28e5bbf03f527421c74fa189866a
ferreira-guilherme-s <souza.guilhermegferreira@gmail.com>
2024-05-27 01:45:58 -0300

    fix

--- api-project/data-source.ts
+++ api-project/data-source.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: 'db',
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
